710104289816c33db189454437744db2f69814d7
minichmari <porcelainmasha@gmail.com>
2025-10-30 17:03:01 +0300

    Параметризация Pytest. Практика работы с pytest параметризацией

--- tools/fakers.py
+++ tools/fakers.py
@@ -1,4 +1,5 @@
 from faker import Faker
+import pytest
 
 
 class Fake:
@@ -28,13 +29,14 @@ class Fake:
         """
         return self.faker.uuid4()
 
-    def email(self) -> str:
+
+    def email(self, domain: str | None = None) -> str:
         """
         Генерирует случайный email.
 
         :return: Случайный email.
         """
-        return self.faker.email()
+        return self.faker.email(domain=domain)
 
     def sentence(self) -> str:
         """
